4b27e8d6fe6070f1a6a7879f2408359a581f438e
Elkin Cobos <68403588+chef28@users.noreply.github.com>
2022-06-16 09:35:19 -0500

    commit1

---
+++ environment/string-data-type.py
@@ -0,0 +1,5 @@
+name = input("Cual es tu nombre? : ")
+color = input("What is your favorite color?  ")
+car = input("What is your favorite car?  ")
+
+print("{}, you like a {} {}!".format(name,color,car))
